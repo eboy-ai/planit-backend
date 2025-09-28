5217f37859904c46e5012b8385e3b2755f426ee1
hyunjun <yygn934@gmail.com>
2025-09-29 01:38:21 +0900

    add services/review.py crudlogic

--- backend/app/db/crud/review.py
+++ backend/app/db/crud/review.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.models import Review
 from app.db.scheme.review import ReviewCreate, ReviewUpdate
-from sqlalchemy import select
+from sqlalchemy import select, or_, desc
 from typing import Optional
 
 class ReviewCrud:
@@ -28,6 +28,39 @@ class ReviewCrud:
     async def get_id(db:AsyncSession, review_id:int) -> Optional[Review]:
         return await db.get(Review, review_id)
     
+    #reveiw-list 조회 trip에속한 리뷰만
+    @staticmethod
+    async def get_all(db:AsyncSession,
+                      trip_id:int,
+                      search:Optional[str]=None,
+                      limit:int=10,
+                      offset:int = 0
+                      ):
+        #데이터선택
+        query = select(Review).where(Review.trip_id == trip_id)
+
+        #검색 기능 조건
+        if search:
+            query = query.where(or_(Review.title.ilike(f'%{search.strip()}%'),
+                                    Review.content.ilike(f'%{search.strip()}%')))
+        #페이지네이션
+        query = query.limit(limit).offset(offset)
+
+        result = await db.execute(query)
+        return result.scalars().all() #rows=result.scalars().all()
+    
+    #Update(review_id)
+    @staticmethod
+    async def update_by_id(db:AsyncSession, review_id:int, review:ReviewUpdate, user_id:int) -> Optional[Review]:
+        db_review = await db.get(Review, review_id)
+        if db_review:
+            update_review = review.model_dump(exclude_unset=True) #review:ReviewUpdate
+            for field, value in update_review.items():
+                setattr(db_review, field, value)
+            await db.flush()
+            return db_review
+        return None
+    
     #Delete
     @staticmethod
     async def delete_by_id(db:AsyncSession, review_id:int) -> bool:
@@ -37,16 +70,7 @@ class ReviewCrud:
             await db.flush()
             return True
         return False
-    
-    #Update(review_id)
-    @staticmethod
-    async def update_by_id(db:AsyncSession, review_id:int, review:ReviewUpdate) -> Optional[Review]:
-        db_review = await db.get(Review, review_id)
-        if db_review:
-            update_review = review.model_dump(exclude_unset=True) #review:ReviewUpdate
-            for field, value in update_review.items():
-                setattr(db_review, field, value)
-            await db.flush()
-            return db_review
-        return None
+
+class LikeCrud:
+    pass
     
---
+++ backend/app/services/review.py
@@ -0,0 +1,82 @@
+from fastapi import HTTPException,status
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.exc import IntegrityError
+from app.db.models import Review
+from app.db.scheme.review import ReviewCreate,  ReviewUpdate
+from app.db.crud import ReviewCrud
+from sqlalchemy import select
+from typing import Optional
+
+
+class ReviewService:
+    #Create
+    @staticmethod
+    async def create(db:AsyncSession, 
+                     review_data:ReviewCreate, 
+                     user_id:int,
+                     trip_id:int):
+        try:
+            db_review = await ReviewCrud.create(db, review_data,user_id,trip_id)
+
+            await db.commit()
+            await db.refresh(db_review)
+            return db_review
+        except Exception:
+            raise
+    
+    #Read
+    #review_id로 조회(R)
+    @staticmethod
+    async def get_id(db:AsyncSession,review_id:int):
+        db_review = await ReviewCrud.get_id(db, review_id)
+
+        if not db_review:
+            raise HTTPException(status_code=404, detail="리뷰가 없습니다")
+        return db_review
+    
+    #trip id에 해당하는 list조회(R)
+    @staticmethod
+    async def get_all_review(db:AsyncSession,
+                      trip_id:int,
+                      search:Optional[str]=None,
+                      limit:int=10,
+                      offset:int = 0):
+        db_review = await ReviewCrud.get_all(db,trip_id,search,limit,offset)
+
+        return db_review
+
+    #Update
+    @staticmethod
+    async def update_review_by_id(db:AsyncSession,review_id:int, review:ReviewUpdate, user_id:int):
+               
+        db_review = await ReviewCrud.get_id(db,review_id)
+        if not db_review:
+            raise HTTPException(status_code=404, detail='리뷰가없습니다')
+        
+        if db_review.user_id != user_id:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        
+        update_data = await ReviewCrud.update_by_id(db, review_id, review,user_id)
+        await db.commit()
+        await db.refresh(update_data)
+        return update_data
+        
+
+    #Delete
+    @staticmethod
+    async def delete_review_by_id(db:AsyncSession, review_id:int):
+        db_review = await ReviewCrud.get_id(db,review_id)
+
+        if not db_review:
+            raise HTTPException(status_code=404, detail='리뷰없음')
+
+        deleted_review = await ReviewCrud.delete_by_id(db,review_id)
+        if deleted_review:            
+            await db.commit()            
+        return {'detail':'리뷰삭제됨'}
+
+
+class LikeService:
+    @staticmethod
+    async def create(db:AsyncSession, user_id:int, review_id:int):
+        pass
