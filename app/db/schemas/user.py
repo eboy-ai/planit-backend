da0b38142ee25e0af5e5417f43ae2ebd78b993fa
hyunjun <yygn934@gmail.com>
2025-10-02 21:13:27 +0900

    directory edit

---
+++ app/db/schemas/user.py
@@ -0,0 +1,37 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional, List
+from app.db.schemas.group import GroupResponse
+
+
+# ----------------------
+# 사용자 관련 스키마
+# ----------------------
+class UserBase(BaseModel):
+    username: str
+    email: EmailStr
+
+class UserCreate(UserBase):
+    password: str
+
+class UserUpdate(BaseModel):
+    username: Optional[str] = None
+    email: Optional[EmailStr] = None
+    password: Optional[str] = None
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserResponse(UserBase):
+    id: int
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+
+    # 🔑 그룹 정보 포함
+    groups: List[GroupResponse] = []
+
+    class Config:
+        orm_mode = True
+        
