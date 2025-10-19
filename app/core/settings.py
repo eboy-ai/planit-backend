b7ebd0a234d0dba6d9691fbf75c000532c822891
youngil.na <kjk00999@gmail.com>
2025-10-19 15:14:38 +0900

    settings,  .env에 OpenWeatherMap API 추가

--- app/core/settings.py
+++ app/core/settings.py
@@ -13,6 +13,10 @@ class Settings(BaseSettings):
     algorithm: str = Field(..., alias="ALGORITHM")
     access_token_expire_minutes: int = Field(..., alias="ACCESS_TOKEN_EXPIRE_MINUTES")
 
+    # OpenWeatherMap API Key
+    open_weather_api_key: str = Field(..., alias="OPEN_WEATHER_API_KEY")
+
+
     class Config:
         env_file = ".env"
         extra = "allow"
