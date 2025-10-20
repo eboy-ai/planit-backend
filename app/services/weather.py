f4d897768bd6ad83c3ed7c69db5a51a930753503
hyunjun <yygn934@gmail.com>
2025-10-21 08:17:06 +0900

    tmp 3hour condition code

--- app/services/weather.py
+++ app/services/weather.py
@@ -1,6 +1,6 @@
 from fastapi import HTTPException,status
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.db.model import Weather, City
+from app.db.model import Weather, City, CityWeather
 from app.db.schema.weather import WeatherBase
 from app.db.crud import WeatherCrud
 from app.services import CityService
@@ -29,7 +29,14 @@ class WeatherService:
         lat = db_city.lat
         lon = db_city.lon
 
+        # 해당 도시의 기존 Weather 조회
+        # if CityWeather.
+        # result = await db.execute(select(Weather).where(Weather.id == db_city.id))
+        # db_weather = result.scalars().first()
 
+        # 3 캐시 유효성 검사
+        # if db_weather and (datetime.utcnow() - db_weather.updated_at) < timedelta(hours=3):
+        #     return json.loads(db_weather.weather_info)
 
         #외부api호출 및 저장(lat,lon)
         url = "https://api.openweathermap.org/data/3.0/onecall"
@@ -46,7 +53,10 @@ class WeatherService:
         #호출 후 DB저장
         await WeatherCrud.create(db,data)
         await db.commit()
-        if response:
-            return data
-        else:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+        #city_weather 중간테이블 저장
+        db.add(CityWeather(city_id=db_city.id, weather_id=db_weather.id))       
+            
+        if response.status_code != 200:
+            raise HTTPException(status_code=502, detail={response.text})
+        return data
+        
